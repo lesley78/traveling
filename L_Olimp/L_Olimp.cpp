44cd7e134326c791f6e740aafa1c1145451ae286
Mr_Fire_ <93352047+MrFire24@users.noreply.github.com>
2022-03-07 21:26:20 +0200

    Update L_Olimp.cpp

--- L_Olimp/L_Olimp.cpp
+++ L_Olimp/L_Olimp.cpp
@@ -1,15 +1,28 @@
 
 #include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+#pragma warning(disable:4996)
 
 using namespace std;
 
 void A1();
 void A2();
 
+
+
 int main()
 {
     A1();
-    
+    while (true) 
+    {
+        Sleep(5);
+        if (kbhit() != 0) break; 
+    }
+
+    A2();
 }
 
 void A1()
