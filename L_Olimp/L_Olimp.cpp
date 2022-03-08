c02a5ca3847071f56daa030db2634a6f9b0c7e5b
Mr_Fire_ <93352047+MrFire24@users.noreply.github.com>
2022-03-08 15:27:49 +0200

    Update L_Olimp.cpp

--- L_Olimp/L_Olimp.cpp
+++ L_Olimp/L_Olimp.cpp
@@ -8,19 +8,169 @@
 
 using namespace std;
 
+
+    //Фунции
+
+int powInt(int N1, int N2) // Без негативных степеней
+{
+    int ret = N1;
+    for (int i = N2; i > 1; i--)
+    {
+        ret = ret * N1;
+    }
+    return ret;
+}
+
+
+int getData(FILE* file_)    //добавить ссылки на массивы из main
+{
+    char str[3];
+    char finder[7];
+    bool isGettingId = false;
+    bool isGettingType = false;
+    bool isGettingX = false;
+    bool isGettingY = false;
+    int id = -1;
+    int i = -1;
+    int objType[10];    //
+    int objX[10];       // !_перенести в main_!  (и оставить на них ссылку в параметрах функции)
+    int objY[10];       //
+
+    while (fgets(str, 2, file_))
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            finder[i] = finder[i+1];
+        }
+        finder[5] = str[0];
+
+        if (isGettingId)
+        {
+
+            if (i == -1 && finder[5] != '"')
+            {
+                i = 1;
+                id = (finder[5] - '0');
+            }
+            else if  (finder[5] != '"')
+            {
+                id = id * 10;
+                id = id + (finder[5] - '0');
+            }
+
+            if (finder[5] == '"')
+            {
+                i = -1;
+                isGettingId = false;
+            }
+        }
+
+        if (isGettingType && id != -1)
+        {
+
+            objType[id] = (finder[5] - '0');
+            isGettingType = false;
+        }
+
+        if (isGettingX)
+        {
+            if (i == -1 && finder[5] != '"')
+            {
+                i = 1;
+                objX[id] = (finder[5] - '0');
+            }
+            else if (finder[5] != '"')
+            {
+                objX[id] = objX[id] * 10;
+                objX[id] = objX[id] + (finder[5] - '0');
+            }
+
+            if (finder[5] == '"')
+            {
+                i = -1;
+                isGettingX = false;
+            }
+        }
+
+        if (isGettingY)
+        {
+            if (i == -1 && finder[5] != '"')
+            {
+                i = 1;
+                objY[id] = (finder[5] - '0');
+            }
+            else if (finder[5] != '"')
+            {
+                objY[id] = objY[id] * 10;
+                objY[id] = objY[id] + (finder[5] - '0');
+            }
+
+            if (finder[5] == '"')
+            {
+                i = -1;
+                isGettingY = false;
+            }
+        }
+
+        if (finder[2] == 'I' && finder[3] == 'D' && finder[4] == ':' && finder[5] == '"')
+        {
+            isGettingId = true;
+            isGettingType = false;
+            isGettingX = false;
+            isGettingY = false;
+        }
+
+        if (finder[0] == 'T' && finder[1] == 'Y' && finder[2] == 'P' && finder[3] == 'E' && finder[4] == ':' && finder[5] == '"')
+        {
+            isGettingId = false;
+            isGettingType = true;
+            isGettingX = false;
+            isGettingY = false;
+        }
+
+        if (finder[3] == 'X' && finder[4] == ':' && finder[5] == '"')
+        {
+            isGettingId = false;
+            isGettingType = false;
+            isGettingX = true;
+            isGettingY = false;
+        }
+
+        if (finder[3] == 'Y' && finder[4] == ':' && finder[5] == '"')
+        {
+            isGettingId = false;
+            isGettingType = false;
+            isGettingX = false;
+            isGettingY = true;
+        }
+        
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "\n" << objType[i] << "\t" << objX[i] << "\t" << objY[i];
+    }
+
+
+    return 0;
+}
+
+
 void A1();
 void A2();
 
-    //Фунции
 
 
 int main()
 {
     //A1();
 
+    //int objType[10];
+    //int objX[10];
+    //int objY[10];
 
-    //A2();
-
+    A2();
+    //cout << powInt(10, 3);
 
 }
 
@@ -28,15 +178,18 @@ int main()
 
 void A1()
 {
-    char str[5];
+    char str[3];
     FILE* file;
     fopen_s(&file, "1.txt", "rt");
 
-    while (fgets(str, 4, file))
+    while (fgets(str, 2, file))
     {
+
         cout << str;
+        Sleep(1000);
     }
     fclose(file);
+
     while (true)
     {
         Sleep(100);
@@ -46,5 +199,8 @@ void A1()
 
 void A2()
 {
+    FILE* file;
+    fopen_s(&file, "2.txt", "rt");
+    cout << "\n\n" << getData(file);
 
 }
