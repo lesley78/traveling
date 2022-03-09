2c2be3201edb5ddf08040ce5dbd660708eab39ad
HellOnEarth19 <100860325+HellOnEarth19@users.noreply.github.com>
2022-03-09 18:57:55 +0200

    Update KoorGame.cpp

--- KoorGame.cpp
+++ KoorGame.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 
 using namespace std;
@@ -6,13 +6,13 @@ using namespace std;
 double getDistance(double latitude1, double longitude1, double latitude2, double longitude2)
 {
 	double radius = 6371, rad = 3.14159265 / 180;
-	double latitude = (latitude2 - latitude1) * rad;
-	double longitude = (longitude2 - longitude1) * rad;
+	double latitude = abs((latitude2 - latitude1)) * rad;
+	double longitude = abs((longitude2 - longitude1)) * rad;
 	double rez = sin(latitude / 2) * sin(latitude / 2) + cos(latitude1 * rad) * cos(latitude2 * rad) * sin(longitude / 2) * sin(longitude / 2);
 	double d = 2 * radius * atan2(sqrt(rez), sqrt(1 - rez));
 	return d;
 }
 int main()
 {
-	cout << "Distance in km " << getDistance(32.17, -73.21, 34.77, -78.1) << endl; // видимо вторую координату надо будет передавать с "-" , дабы не делать модуль в действиях
+	cout << "Distance in km " << getDistance(32.17, 73.21, 34.77, 78.1) << endl;
 }
